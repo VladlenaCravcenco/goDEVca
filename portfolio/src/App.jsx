ae997f1837d5cd70419c003c68dab47b6e10e654
Vladlena <cravcenco97@inbox.ru>
2025-05-18 16:13:50 +0300

    main

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-black-300 font-black text-4xl'>App</div>
+   <section id='home'className='border border-red-500 h-dvh relative text-black-300 px-5 md:p-0'>
+    <div className='w-full h-full flex-center'>
+    <div className='container relative w-full h-full'>
+      <div className='md:mt-40 mt-20'>
+      <p className='font-medium md:text-2xl text-base'>👋🏻 hei, i'm Here</p> 
+      </div>
+    </div>
+    </div>
+   </section>
   )
 }
 
