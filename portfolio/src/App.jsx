dee3903af67e85b5e844bc6c0182e167c3b4f1dd
Vladlena <cravcenco97@inbox.ru>
2025-05-27 18:00:50 +0300

    hero

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import Projects from './sections/Projects';
+import Hero from './sections/Hero';
 
 const App = () => {
   return (
     <main className='text-black'>
-   <section id='home'className='border border-red-500 h-dvh relative text-black-300 px-5 md:p-0'>
-    <div className='w-full h-full flex-center'>
-    <div className='container relative w-full h-full'>
-      <div className='md:mt-40 mt-20'>
-      <p className='font-medium md:text-2xl text-base'>👋🏻 hei, i'm Here</p> 
-      </div>
-    </div>
-    </div>
-   </section>
+    <Hero />
    <Projects />
    </main>
   )
